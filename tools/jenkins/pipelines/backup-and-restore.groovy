f49352d191e33f64a8bc7fb35e370ec06e96a57a
Yanis Guenane <yanis@guenane.org>
2019-01-06 19:27:35 +0100

    dunnowhat

--- tools/jenkins/pipelines/backup-and-restore.groovy
+++ tools/jenkins/pipelines/backup-and-restore.groovy
@@ -2,11 +2,47 @@ pipeline {
 
     agent any
 
+    parameters {
+        choice(
+            name: 'TOWER_VERSION',
+            choices: ['devel', '3.4.0', '3.3.4', '3.3.3', '3.3.2', '3.3.1', '3.3.0',
+                      '3.2.8', '3.2.7', '3.2.6', '3.2.5', '3.2.4', '3.2.3', '3.2.2',
+                      '3.2.1', '3.2.0', '3.1.8']
+        )
+        choice(
+            name: 'ANSIBLE_VERSION',
+            choices: ['devel', 'stable-2.7', 'stable-2.6', 'stable-2.5',
+                      'stable-2.4', 'stable-2.3']
+        )
+        choice(
+            name: 'SCENARIO',
+            choices: ['standalone', 'cluster']
+        )
+        choice(
+            name: 'PLATFORM',
+            choices: ['rhel-7.6-x86_64', 'rhel-7.5-x86_64', 'rhel-7.4-x86_64',
+                      'ol-7.6-x86_64', 'centos-7.latest-x86_64',
+                      'ubuntu-16.04-x86_64', 'ubuntu-14.04-x86_64']
+        )
+        booleanParam(name: 'BUNDLE', defaultvalue: false)
+        string(name: 'TOWERQA_BRANCH', defaultvalue: 'origin/devel')
+        string(name: 'DEPLOYMENT_NAME', defaultvalue: 'jenkins-tower-install')
+        booleanParam(name: 'VERBOSE', defaultvalue: false)
+        booleanParam(name: 'CLEAN_DEPLOYMENT_BEFORE_JOB_RUN', defaultvalue: true)
+        booleanParam(name: 'CLEAN_DEPLOYMENT_AFTER_JOB_RUN', defaultvalue: false)
+        booleanParam(name: 'CLEAN_DEPLOYMENT_ON_JOB_FAILURE', defaultvalue: true)
+    }
+
+    options {
+        timestamps()
+    }
+
     stages {
 
         stage('Display Information') {
             steps {
-                echo "Display Information"
+                echo """Tower Version: ${params.TOWER_VERSION}\n \
+                        Ansible Version: ${params.ANSIBLE_VERSION}"""
             }
         }
 
